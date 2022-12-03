8568225ab5a2acba03d6e9ea748468b7cbe76b65
cludwick <chanceludwick@gmail.com>
2022-12-02 20:56:58 -0600

    Finish for evening

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,82 @@
 import React, { Component } from 'react';
 import './Form.css';
 import InputBase from '../InputBase/InputBase';
+import { OTHERCARDS } from '../constants';
 
 const INIT_CARD = { card: '', cardHolder: '', expiry: '', securityCode: '' };
 
 export default class Form extends Component {
   constructor() {
     super();
-    this.state = { cardData: INIT_CARD, maxLength: 19 };
+    this.state = { cardData: INIT_CARD, maxLength: OTHERCARDS.length, error: {}, cardType: null };
   }
+
+  findDebitCardType = (cardNumber) => {
+    const regexPattern = {
+      MASTERCARD: /^5[1-5][0-9]{1,}|^2[2-7][0-9]{1,}$/,
+      VISA: /^4[0-9]{2,}$/,
+      AMEX: /^3[47][0-9]{5,}$/,
+      DISCOVER: /^6(?:011|5[0-9]{2})[0-9]{3,}$/,
+    };
+    for (const card in regexPattern) {
+      if (cardNumber.replace(/[^\d]/g, '').match(regexPattern[card])) return card;
+    }
+    return '';
+  };
+
+  handleValidation = (type, value) => {
+    switch (type) {
+      case 'card':
+        this.setState({ cardType: this.findDebitCardType(value) });
+        // setState cardType, error
+        break;
+      case 'cardHolder':
+        // checks for spaces and numbers
+        // setState error
+        break;
+      case 'expiry':
+        // check date format
+        // setState error
+        break;
+      case 'securityCode':
+        // check min length
+        // setState error
+        break;
+      default:
+        break;
+    }
+  };
+
+  handleBlur = (e) => this.handleValidation(e.target.name, e.target.value);
+
+  handleInputData = (e) => {
+    if (e.target.name === 'card') {
+      let mask = e.target.value.split(' ').join('');
+      if (mask.length) {
+        mask = mask.match(new RegExp('.{1,4}', 'g')).join(' ');
+        this.setState((prevState) => ({
+          cardData: {
+            ...prevState.cardData,
+            [e.target.name]: mask,
+          },
+        }));
+      } else {
+        this.setState((prevState) => ({
+          cardData: {
+            ...prevState.cardData,
+            [e.target.name]: '',
+          },
+        }));
+      }
+    } else {
+      this.setState((prevState) => ({
+        cardData: {
+          ...prevState.cardData,
+          [e.target.name]: e.target.value,
+        },
+      }));
+    }
+  };
 
   render() {
     const inputData = [
@@ -21,17 +89,20 @@ export default class Form extends Component {
       <div>
         <h1>Add New Card</h1>
         <form action=''>
-          {inputData.map((item) => (
-            <InputBase
-              placeholder={item.label}
-              type={item.type}
-              value={this.state.cardData && this.state.cardData[item.name]}
-              onChange={this.handleInputData}
-              autoComplete='off'
-              maxLength={this.state.maxLength}
-              name={item.name}
-            />
-          ))}
+          {inputData.length
+            ? inputData.map((item) => (
+                <InputBase
+                  placeholder={item.label}
+                  type={item.type}
+                  value={this.state.cardData && this.state.cardData[item.name]}
+                  onChange={this.handleInputData}
+                  autoComplete='off'
+                  maxLength={this.state.maxLength}
+                  name={item.name}
+                  onBlur={this.handleBlur}
+                />
+              ))
+            : null}
           <div className='btn-wrapper'>
             <InputBase type='submit' value='Add Card' />
           </div>
---
+++ src/components/constants.js
@@ -0,0 +1,17 @@
+import VISA_ICON from './assets/visa.png';
+import MASTERCARD_ICON from './assets/masterCard.png';
+import AMEX_ICON from './assets/amex.png';
+import DISCOVER_ICON from './assets/discover.png';
+
+export const OTHERCARDS = [/[1-9]/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/];
+
+export const AMEX = [/[1-9]/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, /\d/];
+
+export const CARD = ['VISA', 'MASTERCARD', 'AMERICAN_EXPRESS', 'DISCOVER'];
+
+export const CARDICON = {
+  VISA: VISA_ICON,
+  MASTERCARD: MASTERCARD_ICON,
+  AMEX: AMEX_ICON,
+  DISCOVER: DISCOVER_ICON,
+};
